835c28d28442a6f9f1af8b04503eed9a8ace15cd
kamabokochan <caffe@harachan.local>
2019-11-04 17:52:38 +0900

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+export default {
+  env: {},
+  head: {
+    title: "nukazuke",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "This is nukazuke" }
+    ],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
+    ]
+  },
+  loading: { color: "#3B8070" },
+  css: ["~/assets/css/main.scss"],
+  build: {},
+  buildModules: ["@nuxt/typescript-build"],
+  modules: [
+    "@nuxtjs/axios",
+  ],
+  axios: {}
+}
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>a</p>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+
+@Component({})
+export default class extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
