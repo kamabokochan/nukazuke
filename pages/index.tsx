2d00f360904ad990edfab43b450df7eb22f44f45
kamabokochan <caffe@harachan.local>
2020-01-03 21:06:56 +0900

    add style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,12 @@
 import "../css/styles.styl"
-
-const linkList = [
-  {
-    name: 'github',
-    url: 'https://github.com/kamabokochan'
-  },
-  {
-    name: 'はてなブログ',
-    url: 'https://h-workfront.hatenablog.com/'
-  },
-  {
-    name: 'Qiita',
-    url: 'https://qiita.com/kamabokochan'
-  },
-  {
-    name: 'Twitter',
-    url: 'https://twitter.com/kamabokochanu'
-  }
-]
+import linkList from "../public/link.js"
 
 export default () =>
   <div className="root">
     <div className="profile-image">
       <img src="kamabokochan.png" alt="" />
     </div>
+    <h2>LINK</h2>
     <ul className="link-list">
       {
         linkList.map((list, index) => <li key={index}><a href={list.url}>{list.name}</a></li>)
---
+++ public/link.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    name: 'github',
+    url: 'https: //github.com/kamabokochan'
+  },
+  {
+    name: 'はてなブログ',
+    url: 'https: //h-workfront.hatenablog.com/'
+  },
+  {
+    name: 'Qiita',
+    url: 'https: //qiita.com/kamabokochan'
+  },
+  {
+    name: 'Twitter',
+    url: 'https: //twitter.com/kamabokochanu'
+  }
+]
